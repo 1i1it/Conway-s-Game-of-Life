bd4b56d3a961f6a14ef1fb65d631b954a6918f57
Lily Mateveyeva <lily.matveyeva@gmail.com>
2016-11-10 20:39:52 +0200

    some unitests

--- testing.rb
+++ testing.rb
@@ -7,7 +7,7 @@ def integration_test(state_one, state_two)
 	my_board = Board.new(state_one)
 	my_board.print_board()
 	my_board.next_turn()
-	new_board = my_board.output_array()
+	new_board = my_board.output_array() #(??)
 	if 
 		my_board.test_array == state_two
 		puts SUCCESS
@@ -31,5 +31,36 @@ def beehive()
 	integration_test(state_one, state_one)
 end
 
+def build_board()
+	my_board = Board.new()
+	length = my_board.output_array().flatten.length
+	if length == 100
+		puts SUCCESS
+	else
+		puts FAIL
+	end
+end
+
+def cell_at_position()
+	pattern = [[0,0,0,0,0],[0,0,0,0,0],[0,1,1,1,0],[0,0,0,0,0],[0,0,0,0,0]]
+	my_board = Board.new(pattern)
+	board = my_board.output_array().flatten
+	if board[11] == 1 && board[7] == 0 
+		puts SUCCESS
+	else 
+		puts FAIL
+	end 
+	my_board.next_turn()
+	new_board = my_board.output_array().flatten
+	if new_board[11] == 0 && new_board[7] ==  1
+		puts SUCCESS
+	else 
+		puts FAIL
+	end 
+end
+
+
+build_board()
+cell_at_position()
 blinker()
 beehive()
