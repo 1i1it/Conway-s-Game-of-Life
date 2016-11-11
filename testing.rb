ed2e22ee94930216b7fb7912f48f0297b89aaf60
Lily Mateveyeva <lily.matveyeva@gmail.com>
2016-11-11 11:29:41 +0200

    adding more tests

--- testing.rb
+++ testing.rb
@@ -16,7 +16,7 @@ def integration_test(state_one, state_two)
 	end
 end
 
-def blinker()
+def blinker_pattern_test()
 	# Oscillator pattern
 	puts "running blinker"
 	state_one = [[0,0,0,0,0],[0,0,0,0,0],[0,1,1,1,0],[0,0,0,0,0],[0,0,0,0,0]]
@@ -24,14 +24,14 @@ def blinker()
 	integration_test(state_one, state_two)
 end
 
-def beehive()
+def beehive_pattern_test()
 	# Still life pattern
 	puts "running beehive"
 	state_one =[[0,0,0,0,0,0],[0,0,1,1,0,0],[0,1,0,0,1,0],[0,0,1,1,0,0],[0,0,0,0,0,0], [0,0,0,0,0,0]]
 	integration_test(state_one, state_one)
 end
 
-def build_board()
+def build_board_test()
 	my_board = Board.new()
 	length = my_board.output_array().flatten.length
 	if length == 100
@@ -41,16 +41,28 @@ def build_board()
 	end
 end
 
-def cell_at_position()
+def cell_at_position_test()
+	puts "running cell_at_position_test"
 	pattern = [[0,0,0,0,0],[0,0,0,0,0],[0,1,1,1,0],[0,0,0,0,0],[0,0,0,0,0]]
 	my_board = Board.new(pattern)
 	board = my_board.output_array().flatten
+
+	#compare to original array, flattened
+	if board == pattern.flatten
+		puts SUCCESS
+	else 
+		puts FAIL
+	end
+
+	
 	if board[11] == 1 && board[7] == 0 
 		puts SUCCESS
 	else 
 		puts FAIL
 	end 
-	my_board.next_turn()
+
+	# play odd number of turns in the game
+	9.times {my_board.next_turn()}
 	new_board = my_board.output_array().flatten
 	if new_board[11] == 0 && new_board[7] ==  1
 		puts SUCCESS
@@ -60,7 +72,11 @@ def cell_at_position()
 end
 
 
-build_board()
-cell_at_position()
-blinker()
-beehive()
+build_board_test()
+cell_at_position_test()
+blinker_pattern_test()
+beehive_pattern_test()
+
+
+# todo check enforcing each law
+
